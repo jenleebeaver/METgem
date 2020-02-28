e66acffcb0e35d56d86a96c71995f7f8fd7f829d
Jen <jen.lee.beaver@gmail.com>
2020-02-28 22:12:05 +0700

    add notes for cli class

--- lib/CLIproject/cli.rb
+++ lib/CLIproject/cli.rb
@@ -2,7 +2,27 @@ class CLIproject::CLI
 
     def start 
         puts "Hello there!"
+        puts "Getting data from API... please wait"
+        puts "Creating new objs"
         binding.pry
     end
+
+    #start should greet user, maybe give desc
+    #get data from scraper/api file
+    #create new custom objs
+    #all inside start method
+
+    # deal with inputs (loop to keep asking to get new info)
+    #EX. while input != "exit" do
+    # display a list of something, or give examples of what we exoect as input 
+    # get user input 
+    # depending on what we get, do something
+    # condition to check input for good value 
+    # else tell them try again
+
+    #exit command
+    # if input == "EXIT"
+    # kill program, say good bye
+
     
 end
